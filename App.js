782d82d6699a964b814a06b88d9c99bf1e9bf37d
CodePicsel <bankarshashank9@gmail.com>
2025-08-05 20:02:32 +0530

    docs: add HTML-formatted README with setup & run steps

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hell yeah waste managment baby!</Text>
+      <Text>Temp project name until we decide actual name of project.</Text>
       <StatusBar style="auto" />
     </View>
   );
